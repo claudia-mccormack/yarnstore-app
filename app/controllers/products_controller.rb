084cb1c00e993ada0343bf7d54e76292a15fa667
Claudia McCormack <mccormack.chc@gmail.com>
2016-07-21 20:23:09 -0400

    Added show pages and links between index and show pages.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,26 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @all_products = Yarn.all
+    render "index.html.erb"
+  end
+
+  def show
+    @yarn = Yarn.find_by(id: params[:id])
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def add_yarn_post
+    Yarn.create(
+      name: params[:name],
+      fiber: params[:fiber],
+      price: params[:price],
+      color: params[:color],
+      image: params[:image]
+    )
+    render "add_yarn.html.erb"
   end
 
   def stroll
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/all_products' => 'products#all_products'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  get '/products/:id' => 'products#show'
+  
+  post '/add_yarn' => 'products#add_yarn_post'
 end
