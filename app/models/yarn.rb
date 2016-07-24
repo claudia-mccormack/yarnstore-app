d481262b99c70a34cc4fbe84d841bc86bf25a951
Claudia McCormack <mccormack.chc@gmail.com>
2016-07-24 16:58:00 -0400

    added tax, total and sale methods

--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -1,2 +1,21 @@
 class Yarn < ActiveRecord::Base
+  def sale_message
+    if price <= 10
+      "Discount Item!"
+    else
+      "On sale!"
+    end
+  end
+
+  def dollars
+    number_to_currency(number)
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
